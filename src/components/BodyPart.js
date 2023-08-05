7bc8856765a7fc829ac19f3b1c7c7b17302b1435
John Kinya <kingjohnkn@gmail.com>
2023-08-05 15:54:46 +0300

    Horizontal Scroll 2

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
 
-const BodyPart = () => {
-  return <div>BodyPart</div>;
+import Icon from "../assets/icons/gym.png";
+
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+        padding: "10px",
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbell"
+        style={{
+          width: "40px",
+          height: "40px",
+        }}
+      />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default BodyPart;
