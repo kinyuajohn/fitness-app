c25b006fc04c33d9660258d5b149c339463700dc
John Kinya <kingjohnkn@gmail.com>
2023-08-07 17:16:12 +0300

    fix horizontal scroll

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -22,6 +22,26 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     window.scrollTo({ top: 1800, behavior: "smooth" });
   };
 
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises(exercisesData);
+    };
+    fetchExerciseData();
+  }, [bodyPart]);
+
   return (
     <Box
       id="exercises"
